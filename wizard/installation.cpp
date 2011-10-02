3464bcb4f23e52bf55d7d60b7af91612cef06e2c
Andreas Loibl <andreas@andreas-loibl.de>
2011-10-02 23:17:35 +0200

    added titles, descriptions and icons to the installation progress list

--- wizard/installation.cpp
+++ wizard/installation.cpp
@@ -39,7 +39,80 @@ void wpInstallation::setProgress(int percent)
 void wpInstallation::receivedCommand(QString command, QString args)
 {
   if(command != "install_step") return;
-  QListWidgetItem *item = new ListItem(args, tr("TODO: use descriptive titles, descriptions and icons in this list..."), "acritoxinstaller");
+  QString title = args, desc, icon = "acritoxinstaller";
+  if(args == "check_partitions_for_install")
+  {
+    title = tr("Checking the installation configuration...");
+    icon = "document-edit-verify";
+  }
+  else if(args == "prepare_target")
+  {
+    title = tr("Setting up the installation target...");
+    icon = "configure";
+  }
+  else if(args == "prepare_partitions_for_install")
+  {
+    title = tr("Preparing partitions for install...");
+    desc = tr("Formatting, setting up mountpoints and mounting partitions according to your settings.");
+    icon = "partitionmanager";
+  }
+  else if(args == "copy_system_to_target")
+  {
+    title = tr("Copying system to installation target...");
+    icon = "edit-copy";
+  }
+  else if(args == "update_fstab_on_target")
+  {
+    title = tr("Updating /etc/fstab on installation target...");
+    icon = "story-editor";
+  }
+  else if(args == "update_passwd_on_target")
+  {
+    title = tr("Setting up users and passwords...");
+    icon = "user-properties";
+  }
+  else if(args == "copy_home_to_target")
+  {
+    title = tr("Copying data for the default user...");
+    desc = tr("The home-directory of the live user is copied to the home-directory of the user on the installation target.");
+    icon = "user-home";
+  }
+  else if(args == "copy_etc_to_target")
+  {
+    title = tr("Copying system settings...");
+    desc = tr("Selected files from /etc are copied to the installation target.");
+    icon = "folder-development";
+  }
+  else if(args == "configure_target_update_files")
+  {
+    title = tr("Updating configuration files...");
+    icon = "run-build-configure";
+  }
+  else if(args == "configure_target_purge_live_only_stuff")
+  {
+    title = tr("Removing live-only stuff...");
+    desc = tr("Removing packages that are only needed on a live system from the installation (e.g. this installer)");
+    icon = "user-trash-full";
+  }
+  else if(args == "configure_target_services")
+  {
+    title = tr("Configuring services...");
+    desc = tr("Setting up the services which get started automatically when booting the installation.");
+    icon = "services";
+  }
+  else if(args == "install_bootmanager_to_target")
+  {
+    title = tr("Installing bootmanager to the selected target...");
+    desc = tr("Generating an initial ramdisk and installing %1 to %2")
+      .arg(backend->cfg("bootloader")).arg(backend->cfg("bootloader_target"));
+    icon = "system-run";
+  }
+  else if(args == "cleanup")
+  {
+    title = tr("Cleaning up...");
+    icon = "run-build-clean";
+  }
+  QListWidgetItem *item = new ListItem(title, desc, icon);
   listWidget->addItem(item);
   listWidget->scrollToItem(item);
   progressCompleted->setValue(progressCompleted->value()+1);

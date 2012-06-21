3e62e6a542308fb50aab52c68ba0e6f217fffff1
Andreas Loibl <andreas@andreas-loibl.de>
2012-06-21 21:43:47 +0200

    added lxdesupport

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <QLocale>
 #include <QLibraryInfo>
 #include <QTranslator>
+#include <QIcon>
+#include "lxdesupport.h"
 #include "mainwizard.h"
 
 int main(int argc, char** argv)
@@ -10,6 +12,8 @@ int main(int argc, char** argv)
     QString locale = QLocale::system().name();
     QTranslator *translator;
     
+    LXDESupport::load(); //load lxde icons
+
     translator = new QTranslator(&app);
     translator->load(QString("qt_")+locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
     app.installTranslator(translator);
